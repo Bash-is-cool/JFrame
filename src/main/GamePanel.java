ea328a36ae4d753a64878766cbca500ef710104e
Bash-is-cool <coolddg@duck.com>
2025-05-15 17:44:10 -0500

    obj array

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -34,7 +34,7 @@ public class GamePanel extends JPanel implements Runnable {
 
     // ENTITY AND OBJECT
     public Player player = new Player(this, keyH);
-    public Entity[] obj = new Entity[10];
+    public Entity[] obj = new Entity[30];
     public Entity[] npc = new Entity[10];
     public Entity[] monster = new Entity[20];
     ArrayList<Entity> entityList = new ArrayList<>();
